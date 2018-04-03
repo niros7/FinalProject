5ab74a5fdd86b01f4b941b8570105a5acf114005
Batush123 <batel.sasson@gmail.com>
2018-04-03 11:05:34 +0300

    add client files

---
+++ angular-realworld-example-app/src/app/app.module.ts
@@ -0,0 +1,66 @@
+import { ModuleWithProviders, NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+/*import { BrowserAnimationsModule } from '@angular/platform-browser/animations';*/
+/*import { MdButtonModule, MdCardModule, MdMenuModule, MdToolbarModule, MdIconModule, MatTabsModule } from '@angular/material';*/
+import {MatTabsModule} from '@angular/material/tabs';
+
+import { AppComponent } from './app.component';
+import { ArticleModule } from './article/article.module';
+import { AuthModule } from './auth/auth.module';
+import { EditorModule } from './editor/editor.module';
+import { HomeModule } from './home/home.module';
+import { ProfileModule } from './profile/profile.module';
+import { SettingsModule } from './settings/settings.module';
+import {
+  ApiService,
+  ArticlesService,
+  AuthGuard,
+  CommentsService,
+  FooterComponent,
+  HeaderComponent,
+  JwtService,
+  ProfilesService,
+  SharedModule,
+  TagsService,
+  UserService,
+  HttpTokenInterceptor
+} from './shared';
+import { FormsModule } from '@angular/forms';
+
+const rootRouting: ModuleWithProviders = RouterModule.forRoot([]);
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    FooterComponent,
+    HeaderComponent
+  ],
+  imports: [
+    BrowserModule,
+    ArticleModule,
+    AuthModule,
+    EditorModule,
+    HomeModule,
+    ProfileModule,
+    rootRouting,
+    SharedModule,
+    /*BrowserAnimationsModule,*/
+    SettingsModule,
+    MatTabsModule,
+  ],
+  providers: [
+    { provide: HTTP_INTERCEPTORS, useClass: HttpTokenInterceptor, multi: true},
+    ApiService,
+    ArticlesService,
+    AuthGuard,
+    CommentsService,
+    JwtService,
+    ProfilesService,
+    TagsService,
+    UserService
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
