c1192c5a249063181aba567412fed0863abfe2b4
Unknown <naor1131@gmail.com>
2018-05-05 18:13:53 +0300

    added auto complete to wizard

--- newClient/src/app/wizard/wizard.component.ts
+++ newClient/src/app/wizard/wizard.component.ts
@@ -25,16 +25,13 @@ export class WizardComponent implements OnInit {
 			if(formSearch != undefined && formSearch != null){
 
 
-				// formResults["country"] =  formSearch.country.value;
-				// formResults["city"] =     formSearch.city.value;
-				// formResults["amount"] =   formSearch.amount.value;
-				// formResults["period"] =   formSearch.period.value;
-				// formResults["drive"] =    formSearch.drive.value;
-				// formResults["walk"] =     formSearch.walk.value;
-				// formResults["romantic"] = formSearch.romantic.value;
-				// formResults["hiking"] =   formSearch.hiking.value;
-				// formResults["ski"] =      formSearch.ski.value;
-				// formResults["nature"] =   formSearch.nature.value;
+				formResults["location"] =  formSearch.location.value;
+				formResults["amount"] =   formSearch.amount.value;
+				formResults["period"] =   formSearch.period.value;
+				formResults["romantic"] = formSearch.romantic.value;
+				formResults["hiking"] =   formSearch.hiking.value;
+				formResults["ski"] =      formSearch.ski.value;
+				formResults["nature"] =   formSearch.nature.value;
 
 				this.submitted = true;
 				console.log(formResults);			
