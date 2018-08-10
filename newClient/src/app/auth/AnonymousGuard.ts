3024c6578062afda5c43f0896d2394705937adad
Batush123 <batel.sasson@gmail.com>
2018-08-10 15:05:08 +0300

    fix bug in login

--- newClient/src/app/auth/AnonymousGuard.ts
+++ newClient/src/app/auth/AnonymousGuard.ts
@@ -24,7 +24,7 @@ export class AnonymousGuard implements CanActivate {
         debugger;
         return new Promise((resolve, reject) => {
             this.userService.isLoggedIn().then(() => {
-                this.router.navigate(['/dashboard']);
+                this.router.navigate(['/about']);
                 reject(false);
             }).catch(() => {
                 resolve(true);
